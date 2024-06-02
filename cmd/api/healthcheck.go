6e95486668143d5e7e97b744a2ce0d26b9c6bc9f
Hyung Jun Noh <munhwas1140@naver.com>
2024-06-02 13:24:28 +0900

    2024-06-02

---
+++ cmd/api/healthcheck.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	js := `{"status": "available", "environment": %q, "version": %q}`
+	js = fmt.Sprintf(js, app.config.env, version)
+	w.Header().Set("Content-Type", "applicadtion/json")
+	w.Write([]byte(js))
+}
